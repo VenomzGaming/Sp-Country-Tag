38d80646f027368923b9b72a993a6b7a1b3819a3
Ayuto22 <ayuto@web.de>
2017-05-17 22:00:05 +0200

    Fixed errors in CS:S
    Fixed an error if the country couldn't be found.
    
    Removed static game checks (used player_activate and try/except
    AttributeError instead).
    
    Clan tags are now set when the plugin is loaded even if players are
    already on the server.
    
    Shortened code.

--- addons/source-python/plugins/country_tag/country_tag.py
+++ addons/source-python/plugins/country_tag/country_tag.py
@@ -1,6 +1,7 @@
 ## IMPORTS
 
 import geoip2.database
+from geoip2.errors import AddressNotFoundError
 
 from core import GAME_NAME
 from events import Event
@@ -21,70 +22,55 @@ from .configs import _configs
 _country_tags = dict()
 _human_players = PlayerIter('human')
 
-## GAME MANAGEMENT
 
-EVENT_CONNECT_GAME = 'player_connect_client'
-
-if GAME_NAME in ('csgo', 'left4dead', 'left4dead2', ):
-    EVENT_CONNECT_GAME = 'player_connect_full'
-
-
-HAS_CLAN_TAG = False
-
-if GAME_NAME in ('csgo', 'cstrike', ):
-    HAS_CLAN_TAG = True
+## LOAD
+def load():
+    for player in _human_players:
+        init_player_clan_tag(player)
+        update_clan_tag(player)
 
 
 ## GAME EVENT
 
-@Event(EVENT_CONNECT_GAME)
-def _on_player_connect_full(event_data):
-    if event_data['userid'] == 0:
-        return
-
+@Event('player_activate')
+def _on_player_activate(event_data):
     player = Player.from_userid(event_data['userid'])
 
-    if player.steamid == 'BOT':
+    if player.is_bot():
         return
 
     if player.userid not in _country_tags:
-        _country_tags[player.userid] = get_country(player.address.split(':', 1)[0])
+        try:
+            tag = init_player_clan_tag(player)
+        except AddressNotFoundError:
+            # TODO: Log a warning?
+            return
 
-    if HAS_CLAN_TAG:
-        update_tag(player)
+        if _configs['connection_announce_steamid'].get_int():
+            CONNECT_STEAMID_ANNOUNCE.send(
+                _human_players,
+                name=player.name,
+                steamid=player.steamid,
+                country=tag.name,
+            )
 
-    if _configs['connection_announce_steamid'].get_int():
-        CONNECT_STEAMID_ANNOUNCE.send(
-            _human_players,
-            name=player.name,
-            steamid=player.steamid,
-            country=_country_tags[player.userid].name,
-        )
+        if _configs['connection_announce'].get_int():
+            CONNECT_ANNOUNCE.send(
+                _human_players,
+                name=player.name,
+                country=tag.name,
+            )
 
-    if _configs['connection_announce'].get_int():
-        CONNECT_ANNOUNCE.send(
-            _human_players,
-            name=player.name,
-            country=_country_tags[player.userid].name,
-        )
-
+    update_clan_tag(player)
 
 @Event('player_disconnect')
 def _on_player_disconnect(event_data):
-    player = Player.from_userid(event_data['userid'])
-
-    if player.userid in _country_tags:
-        del _country_tags[player.userid]
+    _country_tags.pop(event_data['userid'], 0)
 
 
 @Event('player_spawn')
 def _on_player_spawn(event_data):
-    player = Player.from_userid(event_data['userid'])
-    if player.steamid == 'BOT' or player.userid not in _country_tags:
-        return
-
-    if HAS_CLAN_TAG:
-        update_tag(player)
+    update_clan_tag(Player.from_userid(event_data['userid']))
 
 
 ## UTILS
@@ -98,11 +84,23 @@ def get_country(ip):
     return response.country
 
 
-def update_tag(player):
+def update_clan_tag(player):
+    if player.userid not in _country_tags:
+        return
+
     tag = ''
     if _configs['country_tag'].get_int():
         tag = _country_tags[player.userid].iso_code
     elif not _configs['player_tag'].get_int():
         return
 
-    player.clan_tag = tag
+    try:
+        player.clan_tag = tag
+    except AttributeError:
+        pass
+
+
+def init_player_clan_tag(player):
+    result = _country_tags[player.userid] = get_country(
+        player.address.split(':', 1)[0])
+    return result
